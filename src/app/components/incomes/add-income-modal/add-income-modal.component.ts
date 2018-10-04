42c4c9edf7e882042b247fc014f33d61e56cf121
Génesis Salazar <gsalazar@altus.co.cr>
2018-10-03 23:45:57 -0600

    Show add-income-modal

--- src/app/components/incomes/add-income-modal/add-income-modal.component.ts
+++ src/app/components/incomes/add-income-modal/add-income-modal.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-add-income-modal',
+  selector: 'add-income-modal',
   templateUrl: './add-income-modal.component.html',
   styleUrls: ['./add-income-modal.component.css']
 })
 export class AddIncomeModalComponent implements OnInit {
+  @ViewChild('lgModal') public lgModal:ModalDirective;
+  
+  public isModal:boolean = false;
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
 
+
+  hideModal(){
+    this.lgModal.hide();
+  }
+
+  public showModal():void {
+    this.isModal = true;
+  }
+
+  public onHidden():void {
+    this.isModal = false;
+  }
+
+ 
 }
