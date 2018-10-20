25f83d5b510e5304e78c08918145feccf1aab1a0
Génesis Salazar <gsalazar@altus.co.cr>
2018-10-19 18:50:07 -0600

    Add ngModel on bills

--- src/app/components/incomes/add-income-modal/add-income-modal.component.ts
+++ src/app/components/incomes/add-income-modal/add-income-modal.component.ts
@@ -11,10 +11,34 @@ export class AddIncomeModalComponent implements OnInit {
   
   public isModal:boolean = false;
 
+  public bill1000: number;
+  public bill2000: number;
+  public bill5000: number;
+  public bill10000: number;
+  public bill20000: number;
+  public bill50000: number;
+
   constructor() {
    }
 
   ngOnInit() {
+    this.bill1000 = 0;
+    this.bill2000 = 10;
+    this.bill5000 = 0;
+    this.bill10000 = 0;
+    this.bill20000 = 0;
+    this.bill50000 = 0;
+  }
+
+  sumBills(){
+    let sumBill1000 = this.bill1000*1000;
+    let sumBill2000 = this.bill2000*2000;
+    let sumBill5000 = this.bill5000*5000;
+    let sumBill10000 = this.bill10000*10000;
+    let sumBill20000 = this.bill20000*20000;
+    let sumBill50000 = this.bill50000*50000;
+    
+    return  sumBill1000+ sumBill2000+ sumBill5000+ sumBill10000+ sumBill20000+ sumBill50000;
   }
 
 
